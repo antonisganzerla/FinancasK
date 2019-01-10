179eb8bee2db2f5983bac1f91fe5c9738a5587d2
Antoni <antonisganzerla@gmail.com>
2019-01-10 20:56:40 -0200

    iniciado criacao do adapter

--- app/src/main/java/com/antoni/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/antoni/financask/ui/activity/ListaTransacoesActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.antoni.financask.R
+import com.antoni.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
@@ -15,8 +16,8 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         val transacoes = listOf("Comida - R$ 20,50", "Economia - R$  30, 00")
 
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+        val adapter = ListaTransacoesAdapter(transacoes, this)
 
-        lista_transacoes_listview.setAdapter(arrayAdapter)
+        lista_transacoes_listview.setAdapter(adapter)
     }
 }
---
+++ app/src/main/java/com/antoni/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -0,0 +1,34 @@
+package com.antoni.financask.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.antoni.financask.R
+
+class ListaTransacoesAdapter(transacoes: List<String>,
+                             context: Context) : BaseAdapter() {
+
+    private val transacoes = transacoes
+    private val context = context
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
+
+        return view
+    }
+
+    override fun getItem(position: Int): String {
+        return transacoes.get(position)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return transacoes.size
+    }
+
+}
